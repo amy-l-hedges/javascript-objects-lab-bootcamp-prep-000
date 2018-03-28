94cfc78bbfbc0f853740e25e7bcda3ee84bcd153
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-28 22:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function destructivelyUpdateOjbectWithKeyAndValue(object, key, value) {
   return object
 }
 
-function deleteFromObjectBykey(object,key) {
-  
+function deleteFromObjectBykey(object, key) {
+  var newObj = Object.assign({}, object)
+  delete newObj.key
+  return newObj
 }
