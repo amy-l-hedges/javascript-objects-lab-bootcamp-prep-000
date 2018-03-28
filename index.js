1b926b0048fd7c5944690a3c54e609306a7011fb
AmyLou Hedges <amy.l.hedges@gmail.com>
2018-03-28 22:37:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var recipes = {
+  casserole: 'cheese',
+  jello: 'gelatin',
+  deviledEggs: 'mayo'
+} 
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  recipes['pickles'] = 'cucumbers'
+  return recipes
+}
+
+function destructivelyUpdateOjbectWithKeyAndValue(object, key, value) {
+  
+}
